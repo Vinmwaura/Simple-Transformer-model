cca17677c9025c306a5bce97c0155686bea950e6
Vinmwaura <vinmwega@gmail.com>
2024-07-29 04:11:58 +0300

    WIP character embedding dataset loader

---
+++ dataset_loader/character_dataset.py
@@ -0,0 +1,55 @@
+import json
+import string
+import random
+
+import torch
+from torch.utils.data import Dataset
+
+"""
+Loads Character's Embeddings Dataset.
+"""
+class CharacterEmbeddingDataset(Dataset):
+    def __init__(
+            self,
+            dataset_path,
+            context_window=200):
+        with open(dataset_path, "r") as json_f:
+            dataset_dict = json.load(json_f)
+
+        self.dataset = dataset_dict["all"]
+        self.padding_idx = len(dataset_dict["vocab"])
+        self.context_window = context_window
+
+        if len(self.dataset) == 0:
+            raise Exception("No data found.")
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        # Ensures words are not cut off when sampling their characters indices.
+        combined_data = []
+        for i in range(index, len(self.dataset)):
+            combined_data.extend(self.dataset[i])
+
+            if len(combined_data) > self.context_window:
+                break
+
+        if len(combined_data) > self.context_window:
+            input_data = combined_data[0:self.context_window]
+            target_data = combined_data[1:self.context_window+1]
+        else:
+            input_data = combined_data[0:len(combined_data)-1]
+            target_data = combined_data[1:len(combined_data)]
+
+            # Integer index for Pad token, length of vocabulary list.
+            input_pad_index = [self.padding_idx] * (self.context_window - len(input_data))
+            target_pad_index = [self.padding_idx] * (self.context_window - len(target_data))
+
+            input_data.extend(input_pad_index)
+            target_data.extend(target_pad_index)
+
+        input_tensor = torch.tensor(input_data)
+        target_tensor = torch.tensor(target_data)
+
+        return input_tensor, target_tensor
